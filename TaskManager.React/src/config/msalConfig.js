522c5aaf68be346ec83e6cbfaf3b463ce063d8bb
mattosaurus <bitscry@outlook.com>
2022-01-02 10:38:04 +0000

    Add MSAL config

---
+++ TaskManager.React/src/config/msalConfig.js
@@ -0,0 +1,62 @@
+import { LogLevel } from "@azure/msal-browser";
+// Browser check variables
+// If you support IE, our recommendation is that you sign-in using Redirect APIs
+// If you as a developer are testing using Edge InPrivate mode, please add "isEdge" to the if check
+const ua = window.navigator.userAgent;
+const msie = ua.indexOf("MSIE ");
+const msie11 = ua.indexOf("Trident/");
+const msedge = ua.indexOf("Edge/");
+const firefox = ua.indexOf("Firefox");
+const isIE = msie > 0 || msie11 > 0;
+const isEdge = msedge > 0;
+const isFirefox = firefox > 0; // Only needed if you need to support the redirect flow in Firefox incognito
+
+// Config object to be passed to Msal on creation
+export const msalConfig = {
+    auth: {
+        clientId: "a558b305-30f8-45e9-99c0-ed2ec4b453b8",
+        authority: "https://login.microsoftonline.com/8dc638e2-f657-4a1c-bf43-6da5804f5f57",
+        redirectUri: window.location.origin,
+        postLogoutRedirectUri: "/"
+    },
+    cache: {
+        storeAuthStateInCookie: isIE || isEdge || isFirefox
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }
+            }
+        }
+    }
+};
+
+// Add here scopes for id token to be used at MS Identity Platform endpoints.
+export const loginRequest = {
+    scopes: [
+        'api://a558b305-30f8-45e9-99c0-ed2ec4b453b8/user_impersonation'
+    ]
+};
+
+// Add here the endpoints for MS Graph API services you would like to use.
+export const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft-ppe.com/v1.0/me"
+};
